8d8a3302439cc40c5e86b08fd6c865b15c216d77
Rebeca Erdman <71383051+rebecafloriano@users.noreply.github.com>
2024-04-20 14:14:55 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Habilita as exportações estáticas
+  reactStrictMode: true,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
